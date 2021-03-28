dd63992da1711c8af1467bbb786e988ec036f0e0
montanha1337 <arthuratbee@gmail.com>
2021-03-28 18:01:15 -0300

    rotas para consulta de produtos.

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import teste from './controller/teste';
 import user from './controller/user';
+import produtos from './controller/produto'
 
 const routes = new Router();
 
 // rotas modulares verificar como está acessando no arquivo importado
 routes.use('/teste',teste)
 routes.use('/user', user)
+routes.use('/produto',produtos)
 
   module.exports = routes;
