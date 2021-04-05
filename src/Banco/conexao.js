351c1b2888b51be16d300831c00bfca6a333076c
matheusc3m <matheusfernandes3m@hotmail.com>
2021-04-05 20:15:37 -0300

    modificar o retorno do endpoint de produto

--- src/Banco/conexao.js
+++ src/Banco/conexao.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
- 
+
 // criar conexão com a base de dados
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'easyfood',
-  port: '3308',
+  port: '3306',
   namedPlaceholders: true
 });
- //Funçao para testar a conexão
-async function testaconexao(){
-    const teste= await connection.connect();
-    return 'Conexão realizada';
+//Funçao para testar a conexão
+async function testaconexao() {
+  const teste = await connection.connect();
+  return 'Conexão realizada';
 }
 
 
--- src/controller/produto.js
+++ src/controller/produto.js
@@ -3,42 +3,42 @@ import Banco from '../Banco/conexao'
 
 const router = express.Router()
 
-router.post('/cadastro', async(req, res, next)=> {
-   const {descricao,valor,nome,idcategoria,locimagem,idfilial} = req.body
-    const [rows] = await Banco.connection.promise().query('INSERT INTO `produto`(`Descricao`, `Valor`, `Nome`, `IdCategoria`, `LocImagem`, `Bloqueado`, `IdFilial`) VALUES (?,?,?,?,?,0,?)',[descricao,valor,nome,idcategoria,locimagem, idfilial]);
-    const texto = 'id:'+rows.insertId
+router.post('/cadastro', async (req, res, next) => {
+    const { descricao, valor, nome, idcategoria, locimagem, idfilial } = req.body
+    const [rows] = await Banco.connection.promise().query('INSERT INTO `produto`(`Descricao`, `Valor`, `Nome`, `IdCategoria`, `LocImagem`, `Bloqueado`, `IdFilial`) VALUES (?,?,?,?,?,0,?)', [descricao, valor, nome, idcategoria, locimagem, idfilial]);
+    const texto = 'id:' + rows.insertId
     res.status(201).json([texto])
     next();//usado para ficar em standby
 })
-router.get('/buscartodos', async(req, res, next)=>{
+router.get('/buscartodos', async (req, res, next) => {
     const [rows] = await Banco.connection.promise().query('Select * from produto');
-    res.status(200).json(rows)
+    res.status(200).json({ 'produtos': rows })
     next();//usado para ficar em standby
 })
-router.get('/buscarum', async(req, res, next)=>{
-    const {id} = req.body
+router.get('/buscarum', async (req, res, next) => {
+    const { id } = req.body
     const [rows] = await Banco.connection.promise().query('Select * from produto p where p.idproduto=?', [id]);
     res.status(200).json(rows)
     next();//usado para ficar em standby
 })
 
-router.put('/atualizapreco', async(req,res,next)=>{
-    const {produto, preco}=req.body;
-    const [rows] = await Banco.connection.promise().execute('update produto set valor=? where Idproduto=?', [preco,produto]);
+router.put('/atualizapreco', async (req, res, next) => {
+    const { produto, preco } = req.body;
+    const [rows] = await Banco.connection.promise().execute('update produto set valor=? where Idproduto=?', [preco, produto]);
     res.status(200).json("alterado com sucesso");
 })
-router.put('/atualizadescricao', async(req,res,next)=>{
-    const {produto, descricao}=req.body;
-    const [rows] = await Banco.connection.promise().execute('update produto set Descricao=? where Idproduto=?', [descricao,produto]);
+router.put('/atualizadescricao', async (req, res, next) => {
+    const { produto, descricao } = req.body;
+    const [rows] = await Banco.connection.promise().execute('update produto set Descricao=? where Idproduto=?', [descricao, produto]);
     res.status(200).json("alterado com sucesso");
 })
 
-router.delete('/deletaproduto', async(req, res, next)=>{
-    const {produto} = req.body
-    const [rows] = await Banco.connection.promise().execute('delete from produto p where p.idproduto=? ',[produto]);
+router.delete('/deletaproduto', async (req, res, next) => {
+    const { produto } = req.body
+    const [rows] = await Banco.connection.promise().execute('delete from produto p where p.idproduto=? ', [produto]);
     res.status(200).json('Registro apagado')
 })
-router.delete('/apagatodos', async(req, res, next)=>{
+router.delete('/apagatodos', async (req, res, next) => {
 
     const [rows] = await Banco.connection.promise().execute('delete from produto');
     res.status(200).json('Registros apagados')
